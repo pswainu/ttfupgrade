c6dc73fc0b54d2be341169f5fa3e8377ea4752d9
pswainu <prasanjit412@gmail.com>
2021-07-22 16:44:21 +0530

    updated-tag

--- computeengine.tf
+++ computeengine.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "appserver" {
   name = "terraform-upgrade-testing-1"
   machine_type = "f1-micro"
-  tags        = ["test1"]
+  tags        = ["test2"]
   
 
   boot_disk {
