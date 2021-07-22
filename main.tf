9fa08e705cfeb89dc1b6e7510f14167a5610f52b
pswainu <prasanjit412@gmail.com>
2021-07-22 16:03:22 +0530

    3

--- main.tf
+++ main.tf
@@ -3,3 +3,11 @@ provider "google" {
   region = "us-central1"
   zone = "us-central1-a"
 }
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+  required_version = ">= 0.13"
+}
